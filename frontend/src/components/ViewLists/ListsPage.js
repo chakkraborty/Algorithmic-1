0095b782200f7df9c7cfc4d6f85671ec7c644332
arnik <arnikchakraborty50@gmail.com>
2023-05-24 02:41:57 +0530

    myList section frontend

--- frontend/src/components/ViewLists/ListsPage.js
+++ frontend/src/components/ViewLists/ListsPage.js
@@ -4,7 +4,14 @@ import Navbar from "../Navbar/Navbar";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import { useState } from "react";
+import AddListButton from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 const ListsPage = () => {
+  const [l, setl] = useState(false);
+
   return (
     <div>
       <Navbar />
@@ -12,6 +19,22 @@ const ListsPage = () => {
         <div className="myListLeftSection">
           <div className="myListHeader">My List</div>
           <div className="myListItem"> 1. ListItem</div>
+          <div className="myListLeftSectionBottom">
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Name of new list</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter list name"
+                  className="newListNameField"
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Create new list
+              </Button>
+            </Form>
+          </div>
         </div>
         <div className="myListRightSection">
           <div className="myListRightSectionWrapper">
@@ -22,13 +45,20 @@ const ListsPage = () => {
           </div>
           <div className="myListRightSectionItemWrapper">
             <div className="myListRightSectionProblemTitleWrapper">
-              <div className="myListCheckBox">checkBox</div>
+              {/* <div className="myListCheckBox">checkBox</div> */}
+              {!l && <CheckBoxOutlineBlankIcon onClick={() => setl(true)} />}
+              {l && (
+                <CheckBoxIcon
+                  className="myListProblemItemChecker"
+                  onClick={() => setl(false)}
+                />
+              )}
               <div>1.</div>
               <div className="myListRightSectionProblemTitle">
                 Find the longest increasing subsequence
               </div>
-              <DeleteOutlineIcon />
             </div>
+            <DeleteOutlineIcon className="myListDeleteIcon" />
           </div>
         </div>
       </div>

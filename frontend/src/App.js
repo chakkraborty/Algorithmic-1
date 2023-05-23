1e7153c224a1090ae14d2662251eae0b6fa8e4c0
SinghSUBHLEEN <singhcherry9563@gmail.com>
2023-05-23 22:48:48 +0530

    useless4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Login from "./components/screens/login/login";
+import Register from "./components/screens/register/register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Button href="login" variant='primary' size='sm'> Large Button</Button> */}
-        <Login />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Login />
+          <Register />
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 

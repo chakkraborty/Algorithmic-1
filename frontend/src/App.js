29e24eed42ef6ef21db7372e207ba230acdeae56
arnik <arnikchakraborty50@gmail.com>
2023-05-23 21:04:21 +0530

    adding the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
+import "./App.css";
 import Login from "./components/screens/login/login";
-
+import Home from "./components/HomePage/Home.js";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Button href="login" variant='primary' size='sm'> Large Button</Button> */}
-        <Login />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     {/* <Button href="login" variant='primary' size='sm'> Large Button</Button> */}
+    //     <Login />
+    //   </header>
+    // </div>
   );
 }
 

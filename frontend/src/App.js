bf3181893b1ac053e1b1739d10b4d1b16926793b
arnik <arnikchakraborty50@gmail.com>
2023-05-25 01:50:06 +0530

    tempchanges

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,15 @@ import { Routes, BrowserRouter, Route } from "react-router-dom";
 import MyList from "./components/ViewLists/ListsPage.js";
 import Element from "./components/ListElement/listele";
 
-
 const arr = [
   {
     _id: 12324,
     title: "Implement stack",
-    difficulty: "Hard",
+    difficulty: "Medium",
     done: 0,
     topic: "Stack",
-    link: "https://leetcode.com/problems/maximum-subsequence-score/"
-  }
+    link: "https://leetcode.com/problems/maximum-subsequence-score/",
+  },
 ];
 
 export default function App() {
@@ -31,7 +30,19 @@ export default function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route exact path="/register" element={<Register />}></Route>
         <Route exact path="/mylist" element={<MyList />}></Route>
-        <Route exact path="/element" element={<Element topic={arr[0].topic} title={arr[0].title} difficulty={arr[0].difficulty} link={arr[0].link} _id={arr[0]._id} />} />
+        <Route
+          exact
+          path="/element"
+          element={
+            <Element
+              topic={arr[0].topic}
+              title={arr[0].title}
+              difficulty={arr[0].difficulty}
+              link={arr[0].link}
+              _id={arr[0]._id}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );

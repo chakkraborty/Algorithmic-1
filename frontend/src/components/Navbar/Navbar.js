043f9c5d680c2c55648e25b226e6c8f8c9b55d31
arnik <arnikchakraborty50@gmail.com>
2023-05-24 20:54:55 +0530

    this1

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,31 +5,33 @@ import Nav from "react-bootstrap/Nav";
 import NavbarB from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 const Navbar = () => {
-    return (
-        <NavbarB collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
-                <NavbarB.Brand href="#home"><h1 className="Brand">Algorithmic</h1></NavbarB.Brand>
-                <NavbarB.Toggle aria-controls="responsive-navbar-nav" />
-                <NavbarB.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#features">Roadmap</Nav.Link>
-                        <Nav.Link href="#pricing">Articles</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <NavDropdown title="Profile" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">My Lists</NavDropdown.Item>
-                            {/* <NavDropdown.Item href="#action/3.3"></NavDropdown.Item> */}
-                            {/* <NavDropdown.Divider /> */}
-                            {/* <NavDropdown.Item href="#action/3.4">
+  return (
+    <NavbarB collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <NavbarB.Brand href="#home">
+          <h1 className="Brand">Algorithmic</h1>
+        </NavbarB.Brand>
+        <NavbarB.Toggle aria-controls="responsive-navbar-nav" />
+        <NavbarB.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Roadmap</Nav.Link>
+            <Nav.Link href="#pricing">Articles</Nav.Link>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Profile" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">My Lists</NavDropdown.Item>
+              {/* <NavDropdown.Item href="#action/3.3"></NavDropdown.Item> */}
+              {/* <NavDropdown.Divider /> */}
+              {/* <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item> */}
-                        </NavDropdown>
-                    </Nav>
-                </NavbarB.Collapse>
-            </Container>
-        </NavbarB>
-    );
+            </NavDropdown>
+          </Nav>
+        </NavbarB.Collapse>
+      </Container>
+    </NavbarB>
+  );
 };
 
 export default Navbar;

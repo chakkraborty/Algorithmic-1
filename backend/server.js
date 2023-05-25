9f24744183969fda63ad6554d33b3c84ef29bee5
arnik <arnikchakraborty50@gmail.com>
2023-05-25 17:17:43 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
-const express = require('express');
+// const express = require("express");
+// const app = express();
+
+// app.get(["/"], (req, res) => {
+//   res.send("Hello");
+// });
+
+// app.listen(5000, () => {
+//   console.log("Listening at port 5000");
+// });
+
+const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
 
-app.get(["/"], (req, res) => {
-    res.send("Hello");
-})
-
-app.listen(3000, () => {
-    console.log("Listening at port 3000");
-})
+const connectDB = require("./config/db");
+dotenv.config();
+app.use(express.json());
+app.listen(PORT, console.log(`server is running on port no. ${PORT}`));
+connectDB();
+app.get("/", (req, res) => {
+  res.send("api is running here...");
+});

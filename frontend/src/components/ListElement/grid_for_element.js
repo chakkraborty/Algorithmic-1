3c70ef859e202e2c20c99364d8e636f40fd02fbd
arnik <arnikchakraborty50@gmail.com>
2023-05-30 01:39:54 +0530

    ...

--- frontend/src/components/ListElement/grid_for_element.js
+++ frontend/src/components/ListElement/grid_for_element.js
@@ -258,9 +258,9 @@ export default function Grid(props) {
                   {/* <Check
                     type="checkbox"
                     name="rem"
+                    defaultChecked={true}
                     className="bg-inherit"
                     size="lg"
-                    onClick={() => checkHandler(it._id)}
                   /> */}
                   {/* {console.log(typeof it._id)}
                   {myMap[it._id] === 1 ? (
@@ -269,7 +269,7 @@ export default function Grid(props) {
                     <CheckBoxOutlineBlankIcon />
                   )} */}
 
-                  {done.find((c) => c === it._id) ? (
+                  {/* {done.find((c) => c === it._id) ? (
                     <CheckIcon
                       className="gridElementFullBox"
                       onClick={() => validate(it._id)}
@@ -277,6 +277,24 @@ export default function Grid(props) {
                   ) : (
                     <CheckBoxOutlineBlankIcon
                       className="gridElementHollowBox"
+                      onClick={() => validate(it._id)}
+                    />
+                  )} */}
+                  {done.find((c) => c === it._id) ? (
+                    <Check
+                      type="checkbox"
+                      name="rem"
+                      defaultChecked={true}
+                      className="bg-inherit"
+                      size="lg"
+                      onClick={() => validate(it._id)}
+                    />
+                  ) : (
+                    <Check
+                      type="checkbox"
+                      name="rem"
+                      className="bg-inherit"
+                      size="lg"
                       onClick={() => validate(it._id)}
                     />
                   )}
